adf5de241d6c27182f4c3c1730b722abe1283cdb
sinbad <imufun1@gmail.com>
2018-06-11 00:58:06 +0600

    add category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -8,21 +8,23 @@ use Illuminate\Http\Request;
 class CategoryController extends Controller
 {
     //
-	public function addCategory(Request $request){
-	    if ($request->isMethod('post')){
-	        $data = $request->all();
-	     //   echo "<pre>";
-	      //  print_r($data);die();
+    public function addCategory(Request $request)
+    {
+        if ($request->isMethod('post')) {
+            $data = $request->all();
+            //   echo "<pre>";
+            //  print_r($data);die();
 
             $category = new Categories();
-            $category-> category_name  = $data['category_name'];
+            $category->parent_id             = $data['parent_id'];
+            $category->category_name         = $data['category_name'];
             $category->category_description  = $data['Category_description'];
-            $category->category_slug = $data['category_slug'];
-            $category->category_picture  = $data['category_image'];
-            $category->category_status  = $data['category_status'];
+            $category->category_slug         = $data['category_slug'];
+            $category->category_picture      = $data['category_image'];
+            $category->category_status       = $data['category_status'];
             $category->save();
         }
 
-		return view('admin.dashboard.category.init');
-	}
+        return view('admin.dashboard.category.init');
+    }
 }
--- resources/views/admin/dashboard/category/partials/add-category.blade.php
+++ resources/views/admin/dashboard/category/partials/add-category.blade.php
@@ -7,6 +7,22 @@
                     <h4 class="card-title mb-5">Add Category</h4>
                     <form class="form-sample" action="{{url('/admin/add-category')}}" method="post" name="add_category" id="addCategory">
                         {{csrf_field()}}
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group row">
+                                    <label class="col-sm-5 col-form-label">Parent Category</label>
+                                    <div class="col-sm-7">
+                                        <label class="mr-sm-2" for="inlineFormCustomSelect">Select status</label>
+                                        <select class="custom-select mr-sm-2" id="inlineFormCustomSelect" name="parent_id">
+                                            <option selected>Choose...</option>
+                                            <option value="1">Published</option>
+                                            <option value="0">Unpublished</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                        <div class="row">
                             <div class="col-md-6">
                                 <div class="form-group row">

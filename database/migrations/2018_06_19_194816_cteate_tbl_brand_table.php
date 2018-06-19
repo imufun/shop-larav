cf4eba6d272a0e681fe190e247f2a7bf2f5d7230
imran <imufun1@gmail.com>
2018-06-19 12:55:33 -0700

    add brand controller and model

---
+++ database/migrations/2018_06_19_194816_cteate_tbl_brand_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CteateTblBrandTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('tbl_brand', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('brand_name');
+            $table->string('brand_slug');
+            $table->tinyInteger('status');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
